47c0788318114590cf0309bcbb6d1a784aef1fc4
dvacor <dvacor0911@gmail.com>
2021-02-24 19:24:53 +0100

    Pagina index example
    Se actualiza la página index/example con el controlador modificando el abstractController y las annotations

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,18 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController
+class IndexController extends AbstractController
 {
   /**
  * @Route("/index/example")
  */
  public function example():Response
  {
-   return new Response(
-     "<html><body>Example</body></html>"
-   );
+   $example = "Portal Dacoro";
+   return $this->render('index/example.html.twig', [
+            'example' => $example,
+        ]);
  }
 
 }
